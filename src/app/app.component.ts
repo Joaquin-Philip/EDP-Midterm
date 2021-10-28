9ddd001ebe081b47566c9e1e31e14a7101520ea5
Joaquin Philip Tandang <joaquinphilip.tandang@evsu.edu.ph>
2021-10-28 22:05:54 +0800

    more changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'midterm';
+export class AppComponent implements OnInit  {
+
+  currentNav: string = '';
+  observer: any;
+  constructor(private route: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  navClick(current:string){
+    this.currentNav = current;
+  }
+  title='profile';
+
 }
